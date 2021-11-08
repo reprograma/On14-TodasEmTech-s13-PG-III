b1e601493a3bde8777a9d6e10fd8572e9e59e151
Darielly Santos <daryh_cristina@hotmail.com>
2021-11-07 21:55:45 -0300

    feat: criado os endpoints

--- src/controller/noteController.js
+++ src/controller/noteController.js
@@ -1,17 +1,57 @@
-const NoteSchema = require('../models/noteSchema')
+const mongoose = require("mongoose");
+const NoteSchema = require("../models/noteSchema");
 
 const getAll = async (req, res) => {
-    try {
-        const notes = await NoteSchema.find()
-        res.status(200).json(notes)
+  try {
+    const notes = await NoteSchema.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
-    } catch (error) {
-        res.status(500).json({
-            message: error.message,
-        })
+const createNote = async (req, res) => {
+  try {
+    const newNote = new NoteSchema({
+      author: req.body.author,
+      title: req.body.title,
+      _id: new mongoose.Types.ObjectId(),
+    });
+
+    const sevedNote = await newNote.save();
+
+    res.status(200).json({
+      message: "nota adastrada com sucesso!",
+      sevedNote,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const updateNoteById = async (req, res) => {
+  try {
+    const findNote = await NoteSchema.findById(req.params.id);
+
+    if (findNote) {
+      findNote.author = req.body.author || findNote.author;
+      findNote.title = req.body.title || findNote.title;
     }
-}
+
+    const savedNote = await findNote.save();
+    res.status(200).json({
+      message: "Nota atualizada com sucesso",
+      savedNote,
+    });
+  } catch (error) {}
+};
 
 module.exports = {
-    getAll
-}
+  getAll,
+  createNote,
+  updateNoteById,
+};
--- src/routes/noteRoutes.js
+++ src/routes/noteRoutes.js
@@ -1,8 +1,10 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const controller = require("../controller/noteController")
+const controller = require("../controller/noteController");
 
-router.get("/all", controller.getAll)
+router.get("/all", controller.getAll);
+router.post("/create", controller.createNote);
+router.patch("/update/:id", controller.updateNoteById);
 
-module.exports = router
+module.exports = router;
