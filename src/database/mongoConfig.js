df281611514b8e6860589be453504de835a5101a
Darielly Santos <daryh_cristina@hotmail.com>
2021-11-07 21:51:06 -0300

    feat: criada a configuaçao do banco de dados

--- src/database/mongoConfig.js
+++ src/database/mongoConfig.js
@@ -1,19 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const MONGODB_URI = "mongodb+srv://usuario:12345@cluster0.dn30n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const MONGODB_URI =
+  "mongodb+srv://dbBloco:87160862@cluster0.swpfg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const connect = async () => {
-    try {
-        await mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        console.log("Banco conectado (:")
-    } catch (error) {
-        console.log("Erro: ", error.message)
-    }
-}
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Banco conectado (:");
+  } catch (error) {
+    console.log("Erro: ", error.message);
+  }
+};
 
 module.exports = {
-    connect
-}
+  connect,
+};
