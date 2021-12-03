26bdf804957f3a8e858c6cf26c0d4c2f1ffdc04c
Ana Carolliny <80598323+anacarolliny@users.noreply.github.com>
2021-12-03 14:38:29 -0300

    enviando atividades

---
+++ paraLar/src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv-safe").config();
+const db = require("./database/mongoConfig");
+
+const router = require("./routes/moviesRoutes");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/", router);
+
+db.connect();
+
+
+module.exports = app;
